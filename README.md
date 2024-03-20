c76fd5c7fc7e0cb9a2288505a298bc3c5b9aad98
Aart Verbeke <a.verbeke@emakina.com>
2024-03-20 11:16:25 +0100

    improve robustness and error handling

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 
 The technical design is visualized in the design document (design.pdf).
 
-### Design Principles:
+### Key Design Principles:
 - Performance through concurrency
+- Big data by supporting splitted input files
 - Enforce consistency checks upstream
 - Fail fast in case of an error
-- Traceback to enable debugging
+- File position traceback to enable debugging
 
 ### Constraints:
 - A process will generate one schema. To enable generation of different schemas seperate processes need to be instantiated.
 
 ### Not supported yet:
+- Repeated records
+- Constraint relaxation for the "Required" attribute 
 - No handling for the JSON string literal "null"
 - Distribution
 - Schema inference by parsing through repeated records
 - Integration with Google Cloud (Cloud Storage, Bigquery, Dataflow)
-- Incomplete support for Bigquery types: Timestamp, Time, Date
+- Incomplete support for string-wrapped types: Timestamp, Time, Date
 - Test automation 
+- Sampling instead of full file processing
+- Structure in seperate packages
+- Command line interface
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"reflect"
+	"strconv"
 	"sync"
 	"time"
 
@@ -57,7 +57,6 @@ func InferType(value interface{}) (bigquery.FieldType, bool, error) {
 	case bool:
 		return bigquery.BooleanFieldType, false, nil
 	case string:
-		//TODO: try to parse as BigQuery atomic types Timestamp, Time, Date, ...
 		return bigquery.StringFieldType, false, nil
 	case int:
 		return bigquery.IntegerFieldType, false, nil
@@ -66,31 +65,24 @@ func InferType(value interface{}) (bigquery.FieldType, bool, error) {
 	case []interface{}:
 		i, ok := value.([]interface{})
 		if !ok {
-			return bigquery.FieldType(""), false, fmt.Errorf("type assertion error on []interface{}")
-		}
-		// check if all values in the array are of the same type.
-		for u := range i {
-			if u > 1 {
-				if reflect.TypeOf(i[u]) != reflect.TypeOf(i[u-1]) {
-					return bigquery.FieldType(""), false, fmt.Errorf("type inconsistency in repeated field")
-				}
-			}
+			return bigquery.FieldType(""), false, fmt.Errorf("unable to infer type: type assertion error")
 		}
 		switch i[0].(type) {
 		case bool:
 			return bigquery.BooleanFieldType, true, nil
 		case string:
-			//TODO: try to parse as BigQuery atomic types Timestamp, Time, Date, ...
 			return bigquery.StringFieldType, true, nil
 		case int:
 			return bigquery.IntegerFieldType, true, nil
 		case float64:
 			return bigquery.FloatFieldType, true, nil
+		case interface{}:
+			return bigquery.RecordFieldType, true, nil
 		}
 	case interface{}:
 		return bigquery.RecordFieldType, false, nil
 	default:
-		return bigquery.FieldType(""), false, fmt.Errorf("unsupported data type")
+		return bigquery.FieldType(""), false, fmt.Errorf("unable to infer type")
 	}
 	return bigquery.FieldType(""), false, nil
 }
@@ -123,27 +115,46 @@ func TraverseValueMap(schema *Schema, inputMap *map[string]interface{}, trace Tr
 
 		inferredType, repeated, err := InferType(v)
 		if err != nil {
-			return err
+			return fmt.Errorf(err.Error()+" on field: %s", k)
 		}
 
 		if inferredType == bigquery.RecordFieldType && !repeated {
 			nestedSchema := make(Schema, 0)
 			nestedMap, ok := v.(map[string]interface{})
 			if !ok {
-				fmt.Println("type assertion error on map[string]interface{}") //TODO
+				return fmt.Errorf("fatal type assertion on field: %s", k)
 			}
-			err := TraverseValueMap(&nestedSchema, &nestedMap, trace)
+			err := TraverseValueMap(&nestedSchema, &nestedMap, trace) // Recursive call
 			if err != nil {
-				return err
+				return fmt.Errorf(err.Error()+" on field: %s", k)
 			}
 			err = MutexAppend(schema, FieldSchema{Name: k, Type: inferredType, Repeated: repeated, Schema: nestedSchema, Trace: trace})
 			if err != nil {
-				return err
+				return fmt.Errorf(err.Error()+" on field: %s", k)
+			}
+		} else if inferredType == bigquery.RecordFieldType && repeated {
+			nestedSchema := make(Schema, 0)
+			array, ok := v.([]interface{})
+			if !ok {
+				return fmt.Errorf("fatal type assertion on repeated field: %s", k)
+			}
+			nestedMap, ok := array[0].(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("fatal type assertion on repeated field: %s", k)
+			}
+			fmt.Println(nestedMap)
+			err := TraverseValueMap(&nestedSchema, &nestedMap, trace) // Recursive call
+			if err != nil {
+				return fmt.Errorf(err.Error()+" on field: %s", k)
+			}
+			err = MutexAppend(schema, FieldSchema{Name: k, Type: inferredType, Repeated: repeated, Schema: nestedSchema, Trace: trace})
+			if err != nil {
+				return fmt.Errorf(err.Error()+" on field: %s", k)
 			}
 		} else {
 			err = MutexAppend(schema, FieldSchema{Name: k, Type: inferredType, Repeated: repeated, Trace: trace})
 			if err != nil {
-				return err
+				return fmt.Errorf(err.Error()+" on field: %s", k)
 			}
 		}
 
@@ -155,14 +166,17 @@ func Worker(schema *Schema, channel chan Line) {
 	var value interface{}
 	for {
 		line := <-channel
-		json.Unmarshal([]byte(line.TextLine), &value)
+		err := json.Unmarshal([]byte(line.TextLine), &value)
+		if err != nil {
+			panic("fatal unmarshal at position: " + line.Trace.File + " " + strconv.FormatInt(line.Trace.Line, 10))
+		}
 		valueMap, ok := value.(map[string]interface{})
 		if !ok {
-			fmt.Println("type assertion error on map[string]interface{}") //TODO
+			panic("fatal type assertion error at position: " + line.Trace.File + " " + strconv.FormatInt(line.Trace.Line, 10))
 		}
-		err := TraverseValueMap(schema, &valueMap, line.Trace)
+		err = TraverseValueMap(schema, &valueMap, line.Trace)
 		if err != nil {
-			fmt.Println(err) //TODO
+			panic("error: " + err.Error() + " at position: " + line.Trace.File + " " + strconv.FormatInt(line.Trace.Line, 10))
 		}
 	}
 }
@@ -170,7 +184,7 @@ func Worker(schema *Schema, channel chan Line) {
 func ScanFile(fileName string, channel chan Line) {
 	readFile, err := os.Open(fileName)
 	if err != nil {
-		fmt.Println(err) //TODO
+		panic("unable to open file: " + err.Error())
 	}
 	defer readFile.Close()
 	fileScanner := bufio.NewScanner(readFile)
@@ -187,7 +201,8 @@ func main() {
 
 	start := time.Now()
 
-	fileNames := []string{"./benchmark/test1.ndjson", "./benchmark/test2.ndjson", "./benchmark/test3.ndjson", "./benchmark/test4.ndjson", "./benchmark/test5.ndjson"}
+	//fileNames := []string{"./benchmark/test1.ndjson", "./benchmark/test2.ndjson", "./benchmark/test3.ndjson", "./benchmark/test4.ndjson", "./benchmark/test5.ndjson"}
+	fileNames := []string{"./test/test2.ndjson"}
 
 	schema := Schema{}
 	numberOfWorkers := len(fileNames) * 10
@@ -207,6 +222,7 @@ func main() {
 	}
 	wg.Wait()
 
+	time.Sleep(1 * time.Millisecond)
 	for {
 		if len(channel) > 1 {
 			time.Sleep(1 * time.Millisecond)
@@ -219,12 +235,13 @@ func main() {
 
 	d, err := bqSchema.ToJSONFields()
 	if err != nil {
-		fmt.Println(err) //TODO
+		panic("fatal schema conversion to JSON: " + err.Error())
 	}
 
+	fmt.Println("bigquery schema:")
 	fmt.Println(string(d)) //TODO
 
 	elapsed := time.Since(start)
-	fmt.Println("Total Elapsed time: ", elapsed)
-	fmt.Println("Schema Lenght: ", len(bqSchema))
+	fmt.Println("total elapsed time: ", elapsed)
+	fmt.Println("schema lenght: ", len(bqSchema))
 }
